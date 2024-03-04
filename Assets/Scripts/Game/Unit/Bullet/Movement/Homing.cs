e3927891c112a3ced8ed3b6547f080a83ab07a8b
Alter <tuuchan0420@gmail.com>
2024-03-04 17:25:29 +0900

    シーンの作成 プレイヤーの出す弾を一時的にairplaneだけに

---
+++ Assets/Scripts/Game/Unit/Bullet/Movement/Homing.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homing : MonoBehaviour,IBulletMovement
+{
+    float VX = 0;
+    float VY = 0;
+
+    public void Initialize(float speed)
+    {
+        Vector2 velocityVector = Info.Instance.enemyTransform.position - transform.position;
+        velocityVector.Normalize();
+        velocityVector *= speed;
+        VX = velocityVector.x;
+        VY = velocityVector.y;
+    }
+    private void Update()
+    {
+        transform.position = Vector2.right * VX * Time.deltaTime + (Vector2)transform.position;
+        transform.position = Vector2.up * VY * Time.deltaTime + (Vector2)transform.position;
+    }
+    
+
+
+}
