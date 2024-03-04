6b03bc33614946184786cb2c3d422aa3aa18da0f
alt--er <ogasawara.t.ae@m.titech.ac.jp>
2024-03-04 19:49:23 +0900

    ホーミング弾の実装

--- Assets/Scripts/Game/Unit/Bullet/Movement/Homing.cs
+++ Assets/Scripts/Game/Unit/Bullet/Movement/Homing.cs
@@ -5,31 +5,39 @@ using UnityEngine;
 
 public class Homing : MonoBehaviour,IBulletMovement
 {
-    [SerializeField] private Transform chased;
-    float VX = 0;
-    float VY = 0;
+    //将来的には"ホーミング対象を定める(chasedのtransformに上手く追跡対象を入れたいがプレハブ故難しそう?)",
+    //"ユニット側の設定でホーミングの強さを定める"ことができるような設計にしたい
+    
+    private Transform chased;
+    [SerializeField] private float homingforce;
     private float _speed;
     private Rigidbody2D _rb;
+    private Vector2 _positiondiff;
 
     public void Initialize(float speed)
     {
         _speed = speed;
-        Vector2 velocityVector = Info.Instance.enemyTransform.position - transform.position;
-        velocityVector.Normalize();
-        velocityVector *= _speed;
-        _rb.velocity = velocityVector;
+        _positiondiff = chased.position - gameObject.transform.position;
+        _positiondiff.Normalize();
+        _positiondiff *= _speed;
+        _rb.velocity = _positiondiff;
+        Debug.Log(_rb.velocity);
     }
 
     private void Awake()
     {
+        chased = Info.Instance.enemyTransform;
         _rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         //Updateでbulletに力を加える
-        _rb.velocity.Normalize();
-        _rb.velocity *= _speed;
+        _positiondiff = chased.position - gameObject.transform.position;
+        _rb.AddForce(_positiondiff.normalized*homingforce);
+        
+        _rb.velocity=_rb.velocity.normalized*_speed;
+        Debug.Log(_rb.velocity);
     }
     
 
