6fa6a25811dbe08b1a42f1a55b9abadf3adf913f
Synori <i21g.shino@gmail.com>
2024-03-10 21:50:02 +0900

    108

--- Assets/Scripts/Game/Enemy/missiles/Interface/OribitInterface.cs
+++ Assets/Scripts/Game/Enemy/missiles/Interface/OribitInterface.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 public interface IOrbit{
         //軌道用のオブジェクトの取得
-    
-        //バフを受けることができるキャラにつける
         void GameObjectPool(Transform pool,Vector3 vector3,Quaternion quaternion,GameObject missile);
 }
 
