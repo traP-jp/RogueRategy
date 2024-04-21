b3fb4f0f861dbf4a42c3f2219a96ac6a583292c3
ogata <malineog.1268@gmail.com>
2024-04-21 21:25:52 +0900

    もうちょうい作る

--- Assets/Scripts/Game/System/Effect/Contents/AnimationAndDamageFB.cs
+++ Assets/Scripts/Game/System/Effect/Contents/AnimationAndDamageFB.cs
@@ -1,9 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimationAndDamageFB : MonoBehaviour
+namespace Feedback
 {
-    //アニメーションとダメージをまとめたフィードバック
-    //いつか作るかも
+    public class AnimationAndDamageFB : IFeedback
+    {
+        //アニメーションとダメージをまとめたフィードバック
+        //いつか作るかも
+        public void Play(Vector2 position, Action<int> onFinishCallback, int feedbackParallelID = 0)
+        {
+            
+        }
+    }
 }
+
