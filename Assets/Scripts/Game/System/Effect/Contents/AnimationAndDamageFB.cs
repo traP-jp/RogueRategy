2b1e84779ee783f2370d070cd29c56cd99cdef75
ogata <malineog.1268@gmail.com>
2024-04-21 21:24:06 +0900

    アニメーションとダメージ判定のフィードバックを仮置きした

---
+++ Assets/Scripts/Game/System/Effect/Contents/AnimationAndDamageFB.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationAndDamageFB : MonoBehaviour
+{
+    //アニメーションとダメージをまとめたフィードバック
+    //いつか作るかも
+}
