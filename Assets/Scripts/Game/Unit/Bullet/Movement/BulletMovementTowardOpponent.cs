c08cde3e6b7c78aaad59b2ea014e5a03d0050930
ogata <malineog.1268@gmail.com>
2024-07-03 15:18:48 +0900

    エネミー制作途中

--- Assets/Scripts/Game/Unit/Bullet/Movement/BulletMovementTowardOpponent.cs
+++ Assets/Scripts/Game/Unit/Bullet/Movement/BulletMovementTowardOpponent.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletMovementSimple : MonoBehaviour,IBulletMovement
+public class BulletMovementTowardOpponent : MonoBehaviour,IBulletMovement
 {
     float VX = 0;
     float VY = 0;
@@ -24,12 +24,11 @@ public class BulletMovementSimple : MonoBehaviour,IBulletMovement
         VX = velocityVector.x;
         VY = velocityVector.y;
     }
+    
     private void Update()
     {
         transform.position = Vector2.right * VX * Time.deltaTime + (Vector2)transform.position;
         transform.position = Vector2.up * VY * Time.deltaTime + (Vector2)transform.position;
     }
-    
-
 
 }
