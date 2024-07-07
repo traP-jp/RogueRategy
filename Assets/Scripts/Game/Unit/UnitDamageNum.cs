f4d32b485847d32795698f09dc5563bfa655959a
alt--er <ogasawara.t.ae@m.titech.ac.jp>
2024-07-07 22:20:20 +0900

    UnitDamageNum.cs の実装

--- Assets/Scripts/Game/Unit/UnitDamageNum.cs
+++ Assets/Scripts/Game/Unit/UnitDamageNum.cs
@@ -6,31 +6,39 @@ using UnityEngine;
 
 public class UnitDamageNum : MonoBehaviour
 {
-    private TextMeshPro displaydamage;
-    private RectTransform rectTransform;
+    private TextMeshPro _displaydamage;
+    private RectTransform _rectTransform;
+    private Vector3 _initPosition;
     public float DamageNum
     {
         get => DamageNum;
-        set=>displaydamage.text = value.ToString();
+        set=>_displaydamage.text = value.ToString();
     }
 
     public Transform DamagePosition
     {
         set
         {
-            rectTransform.position = (Vector2)Camera.main.WorldToScreenPoint(value.position) + Vector2.up*50;
+            _rectTransform.position = (Vector2)Camera.main.WorldToScreenPoint(value.position) + Vector2.up*50;
+            _displaydamage.rectTransform.position = _rectTransform.position;
+            _initPosition = _rectTransform.position;
         }
     }
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _displaydamage = gameObject.AddComponent<TextMeshPro>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _displaydamage.rectTransform.position+=new Vector3(0f,1f,0f);
+        if (_displaydamage.rectTransform.position.y > _initPosition.y+80f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
