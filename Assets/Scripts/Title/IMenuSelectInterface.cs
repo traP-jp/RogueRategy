40d454eea6205343839f4593b09bfac6aab30ef0
Synori <i21g.shino@gmail.com>
2024-09-30 23:45:34 +0900

    タイトル画面

---
+++ Assets/Scripts/Title/IMenuSelectInterface.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMenuSelectInterface
+{
+    void OnUp();
+    void OnDown();
+    void OnDecide();
+}
